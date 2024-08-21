c80ab3cd8f57cfd90f59fedbc65a906ae6cf511a
ktisa9 <gwarojustus20@gmail.com>
2024-08-21 09:31:38 +0300

    Update settings.js

--- settings.js
+++ settings.js
@@ -10,14 +10,14 @@ const fs = require('fs')
 const chalk = require('chalk')
 
 //contact details
-global.ownernomer = "910000000000"
-global.ownername = "🐛Tech God"
+global.ownernomer = "254111807277"
+global.ownername = "Tylor K9"
 global.ytname = "YT: Tech-God"
 global.socialm = "GitHub: techgod143"
 global.location = "India, Up, Rampur"
 
-global.ownernumber = '917466008456'  //creator number
-global.ownername = '🐛Tech god' //owner name
+global.ownernumber = '254111807277'  //creator number
+global.ownername = 'Tylor K9' //owner name
 global.botname = 'Tech God ᵇᵘᵍ ᵇᵒᵗ' //name of the bot
 
 //sticker details
